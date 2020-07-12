dfad5bd29e71a222a1e2a3c2664a9ccc4610e65e
BW3538 <braden.mcw@gmail.com>
2020-07-12 22:18:56 +0200

    Implemented Job and Technician Classes

---
+++ BusinessLayer/Job.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using DataAccessLayer;
+
+namespace BusinessLayer
+{
+    class Job
+    {
+        DataAccess access = DataAccess.Singleton;
+
+        int id, empID, status, customerProdID;
+        string description;
+        double cost;
+        DateTime scheduledDate;
+
+        public int Id { get => id; set => id = value; }
+        public int EmpID { get => empID; set => empID = value; }
+        public int Status { get => status; set => status = value; }
+        public int CustomerProdID { get => customerProdID; set => customerProdID = value; }
+        public string Description { get => description; set => description = value; }
+        public double Cost { get => cost; set => cost = value; }
+        public DateTime ScheduledDate { get => scheduledDate; set => scheduledDate = value; }
+
+        public Job()
+        {
+
+        }
+
+        public Job(int id, int empID, int status, int customerProdID, string description, double cost, DateTime scheduledDate)
+        {
+            Id = id;
+            EmpID = empID;
+            Status = status;
+            CustomerProdID = customerProdID;
+            Description = description;
+            Cost = cost;
+            ScheduledDate = scheduledDate;
+        }
+    }
+}
---
+++ BusinessLayer/Technician.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using DataAccessLayer;
+
+namespace BusinessLayer
+{
+    public class Technician : Customer
+    {
+        DataAccess access = DataAccess.Singleton;
+        int empID;
+        public int EmpID { get => empID; set => empID = value; }
+        public Technician() { }
+
+        public Technician(int empID, string name, string surname)
+        {
+            EmpID = empID;
+            Name = name;
+            Surname = surname;
+        }
+
+        
+    }
+}
